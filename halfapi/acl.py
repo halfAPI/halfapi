6b999cdb94a4c8d0921ff3474a39a45ebb0653e0
Maxime Alves LIRMM <maxime.alves@lirmm.fr>
2020-07-01 12:05:52 +0200

    sortir connected de baseACL

--- halfapi/acl.py
+++ halfapi/acl.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
+def connected(func):
+    """ Decorator that checks if the user object of the request has been set
+    """
+    def caller(req, *args, **kwargs):
+        try:
+            getattr(req.user, 'is_authenticated')
+            return func(req, **kwargs)
+        except AttributeError:
+            return False
+
+    return caller
+
 class BaseACL:
     """ Base ACL class that contains generic methods for domains ACL
     """
-    def connected(req, func):
-        """ Decorator that checks if the user object of the request has been set
-        """
-        def caller():
-            try:
-                getattr(req.user, 'is_authenticated')
-                return func()
-            except AttributeError:
-                return False
-
-        return caller
-
     def public(self, *args) -> bool:
         "Unlimited access"
         return True
