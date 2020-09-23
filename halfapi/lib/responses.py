d6075de2ebdc552551a7355a3a268ba157854891
Maxime Alves LIRMM <maxime.alves@lirmm.fr>
2020-09-23 10:56:32 +0200

    [lib][responses] add ORJSONResponse and update HJSONResponse

--- halfapi/lib/responses.py
+++ halfapi/lib/responses.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # builtins
-import numbers
-import csv
-from datetime import date
-from io import TextIOBase, StringIO
+import orjson
 from half_orm.null import NULL
 
 # asgi framework
 from starlette.responses import PlainTextResponse, Response, JSONResponse
+
 
 __all__ = [
     'InternalServerErrorResponse',
@@ -44,26 +42,12 @@ class UnauthorizedResponse(Response):
     def __init__(self, *args, **kwargs):
         super().__init__(status_code = 401)
 
-class HJSONResponse(JSONResponse):
-    def __init__(self, obj):
-        obj = self.__serialize(obj)
-        super().__init__(
-            content=obj,
-            status_code = 200)
 
-    def __serialize(self, obj):
-        if isinstance(obj, dict):
-            robj = dict()
-            for key, value in obj.items():
-                robj[key] = self.__serialize(value)
-            return robj
-        if isinstance(obj, list):
-            robj = []
-            for value in obj:
-                robj.append(self.__serialize(value))
-            return robj
-        if isinstance(obj, numbers.Number) or isinstance(obj, str):
-            return obj
-        if obj == NULL:
-            return None
-        return str(obj)
+class ORJSONResponse(JSONResponse):
+    def render(self, content: typ.Any) -> bytes:
+        return orjson.dumps(content)
+
+
+class HJSONResponse(ORJSONResponse):
+    def render(self, content: typ.Generator):
+        return super().render([ elt for elt in content ])
