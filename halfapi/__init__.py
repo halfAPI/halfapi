d21ee175e9504e99778a58770b08e3281deed5b5
Maxime Alves LIRMM <maxime.alves@lirmm.fr>
2021-01-14 10:55:02 +0100

    [responses] cast special types in ORJSONResponse

--- halfapi/__init__.py
+++ halfapi/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = '0.3.1'
+__version__ = '0.4.1'
 
 def version():
     return f'HalfAPI version:{__version__}'
--- halfapi/lib/responses.py
+++ halfapi/lib/responses.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # builtins
+import decimal
 import typing as typ
 import orjson
 
@@ -47,13 +48,24 @@ class UnauthorizedResponse(Response):
 
 class ORJSONResponse(JSONResponse):
     def __init__(self, content, default=None, **kwargs):
-        self.default = default
+        self.default = default if default is not None else ORJSONResponse.default_cast
         super().__init__(content, **kwargs)
 
     def render(self, content: typ.Any) -> bytes:
         return orjson.dumps(content,
             option=orjson.OPT_NON_STR_KEYS,
             default=self.default)
+
+    @staticmethod
+    def default_cast(x):
+        types = {
+            decimal.Decimal
+        }
+
+        if type(x) not in types:
+            raise TypeError('Type is not handled in ORJSONResponse')
+
+        return str(x)
 
 
 class HJSONResponse(ORJSONResponse):
