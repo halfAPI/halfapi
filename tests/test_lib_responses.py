54e215b6ff2f3f7981d88570d7df1370c943ee30
Maxime Alves LIRMM@home <maxime.alves@lirmm.fr>
2021-01-23 09:30:53 +0100

    [test][nf] added basic test methods for orjsonresponse

---
+++ tests/test_lib_responses.py
@@ -0,0 +1,21 @@
+import json
+import decimal
+
+from halfapi.lib.responses import ORJSONResponse
+
+
+def test_orjson():
+    test_obj = {
+        "ok": "ko",
+        "dec": decimal.Decimal(42),
+        "set": set([0,4,2])
+    }
+
+    resp = ORJSONResponse(test_obj)
+    body = resp.body.decode()
+    test_obj_dec = json.loads(body)
+    print(test_obj_dec)
+    assert 'ok' in test_obj_dec.keys()
+    assert isinstance(test_obj_dec['ok'], str)
+    assert isinstance(test_obj_dec['dec'], str)
+    assert isinstance(test_obj_dec['set'], liststr)
