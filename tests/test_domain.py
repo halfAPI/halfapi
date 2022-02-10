c3153921f77ca66cebb64c793ed5002e1ea8b09d
Maxime Alves LIRMM <maxime.alves@lirmm.fr>
2022-02-10 11:59:40 +0100

    [tests] test_domain import dummy_domain inside test class

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,8 +1,9 @@
 from halfapi.testing.test_domain import TestDomain
-from dummy_domain import __name__, __routers__
 from pprint import pprint
 
 class TestDummyDomain(TestDomain):
+    from .dummy_domain import __name__, __routers__
+
     DOMAIN = __name__
     ROUTERS = __routers__
 
